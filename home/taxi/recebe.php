d845491ecca650dfd5e707c699158120fc4f8fa6
André Gomes da Silva <andregds85@gmail.com>
2021-09-05 06:14:41 -0300

    teste
    teste

---
+++ home/taxi/recebe.php
@@ -0,0 +1,19 @@
+<?php
+/* criar um vetor Vazio */ 
+  $destino = array();
+  $pagamento =array();
+  $valor =array();
+
+
+  $destino[]=$_POST["destino"];
+  $pagamento[]=$_POST["pagamento"];
+  $valor[]=$_POST["valor"];
+
+  
+
+  $total = $valor[]=$_POST["valor"];
+  echo "soma(a) = ".array_sum($total)."\n";
+
+
+
+?>
